c9dbacaa9a9f9c9d50b85cece2d75842f7377c59
Vaja Bhavin <vajabhavin96@gmail.com>
2026-03-26 12:31:44 +0000

    Student Registetion Added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,12 +9,28 @@ def home():
 def about():
     return render_template("about.html")
 
-@app.route("/student")
-def student():
+@app.route("/student/home")
+def studentHome():
     return render_template("student.html")
 
 @app.route("/student/givefeedback")
 def givefeedback():
     return render_template("givefd.html")
+
+@app.route("/faculty/home")
+def facultyHome():
+    return render_template("faculty.html")
+
+@app.route("/submit",methods=["POST"])
+def submit():
+    en = request.form.get("en")
+    gmail = request.form.get("gmail")
+    print("En:",en)
+    print("Gmail:",gmail)
+    return render_template("student.html")
+
+@app.route("/student/register")
+def studentRegister():
+    return render_template("student-register.html")
 
 app.run(debug=True)
