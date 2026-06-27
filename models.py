4d5645f3eb55fcc34403b5e3b4a5fb27c1ff8125
Vaja Bhavin <vajabhavin96@gmail.com>
2026-06-27 15:40:44 +0000

    logout feature added.

--- models.py
+++ models.py
@@ -7,3 +7,23 @@ class Student(db.Model):
     sem = db.Column(db.Integer)
     div = db.Column(db.String(1))
     password = db.Column(db.String(8))
+
+class Collage(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    college_code = db.Column(db.String(20), unique=True, nullable=False)
+    college_name = db.Column(db.String(150), nullable=False)
+
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    contact_no = db.Column(db.String(15), nullable=False)
+
+    address = db.Column(db.Text, nullable=False)
+
+    university = db.Column(db.String(150))
+    website = db.Column(db.String(150))
+
+    password = db.Column(db.String(255), nullable=True)
+
+    verified = db.Column(db.Boolean, default=False)
+
+    created_at = db.Column(db.DateTime, default=db.func.current_timestamp())
