08fb74d9cd3ea5cb4c61f584346085475a23209d
Vaja Bhavin <vajabhavin96@gmail.com>
2026-06-27 23:08:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,8 +39,13 @@ app.register_blueprint(student)
 app.register_blueprint(faculty)
 app.register_blueprint(collage)
 
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
 
-app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=True
+    )
